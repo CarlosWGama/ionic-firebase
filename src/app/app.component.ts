af7c41aec59cfc232cc88488ebedff362fb17cf0
Carlos W. Gama <carloswgama@gmail.com>
2017-01-26 21:05:21 -0300

    Menu Lateral

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,49 @@
-import { HomePage } from './../pages/home/home';
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, MenuController, NavController, NavOptions } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { LoginPage } from './../pages/login/login';
+import { HomePage } from './../pages/home/home';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage = LoginPage;
-  pages: { page: any, titulo: string }[] = [
-    { page: HomePage, titulo: "Perguntar"}
-  ]
+  pages: { page: any, titulo: string, icon: string }[] = [
+    { page: HomePage, titulo: "Minhas Perguntas", icon: "help"},
+    { page: HomePage, titulo: "Meus conselhos", icon: "ios-heart-outline"},
+    { page: HomePage, titulo: "Discuta", icon: "ios-megaphone-outline"},
+  ];
 
-  constructor(platform: Platform) {
+  // Navegação 
+  @ViewChild("content") navCtrl: NavController;
+  navOptions: NavOptions = {animate: true, animation: 'wp-transition', direction: 'forward', duration: 2000}
+
+  constructor(platform: Platform, public menuCtrl: MenuController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
+      this.menuCtrl.enable(false);
     });
+  }
+
+  /**
+   * Troca entre as diferentes páginas
+   */
+  menuTrocaPage(page: any) {
+    this.navCtrl.setRoot(page, null, this.navOptions)
+  }
+
+  /**
+   * Realiza o Logout do APP
+   */
+  menuLogout() {
+    this.menuCtrl.close();
+    this.menuCtrl.enable(false);
+    this.navCtrl.setRoot(LoginPage, null, this.navOptions)
   }
 
  
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,6 @@
+import { HomePage } from './../home/home';
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavOptions, MenuController } from 'ionic-angular';
 
 /*
   Generated class for the Login page.
@@ -14,15 +15,35 @@ import { NavController, NavParams } from 'ionic-angular';
 export class LoginPage {
 
   cadastrar: boolean = true;
+  private navOptions: NavOptions = { animate: true, animation: 'md-transition', direction: 'forward' }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public menuCtrl: MenuController) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
   }
 
+  /**
+   * Altera o tipo de ação da tela
+   */
   alterarBotoes(realizarCadastro: boolean) {
     this.cadastrar = realizarCadastro;
+  }
+
+  /**
+   * Realiza o cadastro do usuário
+   */
+  cadastro() {
+    this.menuCtrl.enable(true);
+    this.navCtrl.setRoot(HomePage, this.navOptions);
+  }
+
+  /**
+   * Realiza o login do usuário
+   */
+  login() {
+    this.menuCtrl.enable(true);
+    this.navCtrl.setRoot(HomePage, this.navOptions);
   }
 
 }
